84cf9bab414c0e002995235be5d53ba67fe2ad29
Prateek-saraswat <saraswatprateek0@gmail.com>
2025-12-18 18:38:40 +0530

    Login , signup ui improved

--- my-react-app/components/Navbar.jsx
+++ my-react-app/components/Navbar.jsx
@@ -1,6 +1,4 @@
-import React from 'react';
 import '../styles/Navbar.css';
-// import { useAuth } from '../context/useAuth';
 
 export default function Navbar({useAuth}) {
   const { user, logout } = useAuth();
